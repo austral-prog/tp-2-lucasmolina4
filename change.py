f65e97690ebf2916751d2776c847cad96e14182d
lucasmolina4 <162722811+lucasmolina4@users.noreply.github.com>
2024-03-22 14:28:34 -0300

    TP2 change.py

--- change.py
+++ change.py
@@ -1,3 +1,8 @@
 def change():
     expense = 23.75
     money = 100
+print (f"Ingresar gasto:\n{expense}")
+print (f"Dinero recibido\n{money}\n")
+print ("Vuelto\n")
+print (f"Pesos:\n{int(money-expense)}")
+print (f"Centavos:\n{int(((money-expense)-int(money-expense))*100)}")
